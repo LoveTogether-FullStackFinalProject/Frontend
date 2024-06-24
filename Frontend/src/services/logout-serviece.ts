66f456dcc1b8314698780fe55f36710c4826f50b
shaynoadotan <118252959+shaynoadotan@users.noreply.github.com>
2024-06-24 16:33:02 +0300

    logout serviece

---
+++ Frontend/src/services/logout-serviece.ts
@@ -0,0 +1,36 @@
+
+import apiClient from "./api-client"
+
+
+export interface IUser {
+  email: string,
+  password?: string,
+  image?: string,
+  _id?: string,
+  accessToken?: string,
+  refreshToken?: string
+}
+
+
+export const postLogout = async () => {
+  const abortController = new AbortController()
+  const refreshToken = localStorage.getItem("refreshToken");
+  if (!refreshToken) {
+      throw new Error("No refresh token found");
+  }
+  console.log( refreshToken);
+  await apiClient.get('auth/logout', { 
+      headers: {
+          'Authorization': `Bearer ${refreshToken}`
+      },
+      signal: abortController.signal
+   });
+   localStorage.removeItem('accessToken');
+   localStorage.removeItem('refreshToken');
+   localStorage.removeItem('userID');
+
+  };
+
+  
+
+export default {postLogout}

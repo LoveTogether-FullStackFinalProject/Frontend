8af0ef7a290566b6c6a55967ca629103e387cd9d
Michal Massachi <michal2792000@gmail.com>
2024-07-26 14:04:11 +0300

    moving to login from upload product if user not logged in

--- Frontend/src/components/UploadProduct.tsx
+++ Frontend/src/components/UploadProduct.tsx
@@ -178,17 +178,18 @@ const UploadProduct: React.FC = () => {
  
 
   if (!isLoggedIn) {
-    return (
-      <div className="error-container">
-        <div className="error-title">תרומת מוצר - עמותת ואהבתם ביחד</div>
-        <div className="error-message">
-          על מנת להעלות פריט לתרומה, יש לבצע הרשמה לאתר
-        </div>
-        <button className="login-button" onClick={() => navigate('/login')}>
-          Login
-        </button>
-      </div>
-    );
+    navigate('/login')
+    // return (
+    //   <div className="error-container">
+    //     <div className="error-title">תרומת מוצר - עמותת ואהבתם ביחד</div>
+    //     <div className="error-message">
+    //       על מנת להעלות פריט לתרומה, יש לבצע הרשמה לאתר
+    //     </div>
+    //     <button className="login-button" onClick={() => navigate('/login')}>
+    //       Login
+    //     </button>
+    //   </div>
+    // );
   }
 
   return (

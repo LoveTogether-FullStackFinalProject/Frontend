c1aade81be8a0c709ac06f65914933697d251247
Michal Massachi <michal2792000@gmail.com>
2024-07-27 17:33:01 +0300

    edit requested product page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -12,6 +12,7 @@ import UploadProduct from './components/UploadProduct';
 import  UploadRequestedProduct  from './components/upload-requested-product';
 import ManageRequestedDonations from './components/ManageRequestedDonations';
 import ManageMainPageUsers from './components/ManageMainPageUsers';
+import EditRequestedProduct from './components/edit-requested-product';
 import { Navbar } from "./components//Navbar";
 import { useState } from 'react';
 import { User } from './services/types';
@@ -48,7 +49,8 @@ const App = () => {
                 <Route path="/manageDonations" element={<ManageDonationPage />} />
                 <Route path="/manageUsers" element={<ManageUsers />} />   
                 <Route path="/manageRequestedDonations" element={<ManageRequestedDonations />} />
-                <Route path="/manageMainPageUsers" element={<ManageMainPageUsers />} />                          
+                <Route path="/manageMainPageUsers" element={<ManageMainPageUsers />} /> 
+                <Route path="/editRequestedProduct" element={<EditRequestedProduct />} />                          
             </Routes>
         </Router>
         </div>
--- Frontend/src/components/AdminDashboard.tsx
+++ Frontend/src/components/AdminDashboard.tsx
@@ -60,8 +60,10 @@ const AdminPage = () => {
         <button className="admin-button" onClick={() => handleButtonClick('/statistics')}>דוחות נתונים</button>
         <button className="admin-button" onClick={() => handleButtonClick('/manageUsers')}>נהל יוזרים</button>
         <button className="admin-button" onClick={() => handleButtonClick('/uploadRequestedProduct')}>העלאת בקשת פריט</button>
+        {/* <button className="admin-button" onClick={() => handleButtonClick('/editRequestedProduct')}>עריכת בקשת פריט</button> */}
         <button className="admin-button" onClick={() => handleButtonClick('/manageRequestedDonations')}> נהל תרומות שהעמותה מבקשת</button>
         <button className="admin-button" onClick={() => handleButtonClick('/manageMainPageUsers')}>נהל הצגת תורמים בעמוד הראשי</button>
+        
       </div>
     </div>
   );

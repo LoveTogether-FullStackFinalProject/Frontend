257ffa112336adb3070e8e88c9b60482febf2b02
Michal Massachi <michal2792000@gmail.com>
2024-08-10 10:37:15 +0300

    update url

--- Frontend/src/services/api-client.ts
+++ Frontend/src/services/api-client.ts
@@ -4,7 +4,7 @@ export { CanceledError }
 const apiClient = axios.create({
     
    // baseURL: 'http://localhost:3000',
-   baseURL: 'http://ve-be.cs.colman.ac.il'
+   baseURL: 'https://ve-be.cs.colman.ac.il'
 });
 
 export default apiClient;

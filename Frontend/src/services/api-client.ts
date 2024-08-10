b13408e6a7ed1d37d0c58cdb3132565e30f587bd
chenAmrani <chenamrani5@gmail.com>
2024-08-10 12:09:55 +0300

    update baseURL

--- Frontend/src/services/api-client.ts
+++ Frontend/src/services/api-client.ts
@@ -2,8 +2,6 @@ import axios, { CanceledError } from "axios";
 
 export { CanceledError }
 const apiClient = axios.create({
-    
-   // baseURL: 'http://localhost:3000',
    baseURL: 'https://node12.cs.colman.ac.il'
 });
 

04e3403c8a14e048b128fad7005f3d92c3281ff4
Michal Massachi <michal2792000@gmail.com>
2024-08-20 21:30:52 +0300

    upload requested product errors style

--- Frontend/src/components/upload-requested-product.tsx
+++ Frontend/src/components/upload-requested-product.tsx
@@ -141,6 +141,11 @@ function UploadRequestedProduct() {
                   autoFocus
                   error={!!errors.itemName}
                   helperText={errors.itemName?.message}
+                  FormHelperTextProps={{
+                    sx: {
+                      marginLeft: '260px', 
+                    },
+                  }}
                   InputLabelProps={{
                     sx: {
                       right: 19,
@@ -185,6 +190,11 @@ function UploadRequestedProduct() {
                   }}
                   error={!!errors.category}
                   helperText={errors.category?.message}
+                  FormHelperTextProps={{
+                    sx: {
+                      marginLeft: '260px', 
+                    },
+                  }}
                   InputLabelProps={{
                     sx: {
                       right: 19,
@@ -232,6 +242,11 @@ function UploadRequestedProduct() {
                     label="הזן קטגוריה"
                     error={!!errors.customCategory}
                     helperText={errors.customCategory?.message}
+                    FormHelperTextProps={{
+                      sx: {
+                        marginLeft: '270px', 
+                      },
+                    }}
                     sx={{ mt: 2 }}
                     InputLabelProps={{
                       sx: {
@@ -272,6 +287,11 @@ function UploadRequestedProduct() {
                   label="כמות"
                   error={!!errors.amount || !!amountError}
                   helperText={errors.amount?.message || amountError}
+                  FormHelperTextProps={{
+                    sx: {
+                      marginLeft: '280px', 
+                    },
+                  }}
                   type="number"
                   InputLabelProps={{
                     sx: {
@@ -308,6 +328,11 @@ function UploadRequestedProduct() {
                   label="מצב המוצר"
                   error={!!errors.itemCondition}
                   helperText={errors.itemCondition?.message}
+                  FormHelperTextProps={{
+                    sx: {
+                      marginLeft: '260px', 
+                    },
+                  }}
                   InputLabelProps={{
                     sx: {
                       right: 19,
@@ -343,6 +368,11 @@ function UploadRequestedProduct() {
                   label="תיאור"
                   error={!!errors.description}
                   helperText={errors.description?.message}
+                  FormHelperTextProps={{
+                    sx: {
+                      marginLeft: '250px', 
+                    },
+                  }}
                   InputLabelProps={{
                     sx: {
                       right: 19,
